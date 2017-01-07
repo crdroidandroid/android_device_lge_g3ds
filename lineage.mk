5aa1643034c6da05aa6798d5782b75aa4422de95
firebird11 <hbgassel@gmail.com>
2017-01-07 11:23:49 +0100

    g3ds: rebrand to Lineage
    Change-Id: I89ed8adf9e5bfc0255810848f0a32d141472f24a

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/g3ds/full_g3ds.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_g3ds
+PRODUCT_NAME := lineage_g3ds
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3"

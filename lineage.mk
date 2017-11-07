c134fd50808a8b11a665e7f8ee2b3bdad6bff872
firebird11 <hbgassel@gmail.com>
2017-11-07 23:26:06 +0100

    vendor cm to lineage
    Change-Id: I8484c0dfb4a0e771ef98244e7184e119877dc7ab

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/lge/g3ds/full_g3ds.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_g3ds
 

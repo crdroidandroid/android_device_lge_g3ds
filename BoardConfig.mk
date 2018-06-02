e88ed27243672584457ac8c665428bbd28062ecd
Paul Keith <javelinanddart@gmail.com>
2018-06-02 09:26:50 +0200

    g3ds: Use sdfat for exfat and vfat
    Change-Id: Ifb16dd6282d5f6b2521b5a5c504d5732c14ba9fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/g3ds/bluetooth
 BOARD_CUSTOM_BT_CONFIG := device/lge/g3ds/bluetooth/vnd_g3.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
+# Extended Filesystem Support
+TARGET_EXFAT_DRIVER := sdfat
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_dualsim_defconfig
 TARGET_REQUIRES_BUMP := true

cca396e60d70739beffbe7589159efe16059749b
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-09-09 17:27:49 +0200

    g3ds: Add bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "LG G3"
+#define BTM_DEF_LOCAL_NAME "LG G3 (G3DS)"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #define BLE_VND_INCLUDED TRUE

2ea5ee1c045a482a2c56a29946de718eca7a8fc1
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-06-22 13:21:28 +0200

    g3ds: Rework dual-sim support
    Change-Id: Id55385f5a587a55eb25050da93963a0311abf103

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,3 +10,18 @@ LOCAL_SRC_FILES         := etc/fstab.g3
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.class_main.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.class_main.sh
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.g3ds.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.g3ds.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)

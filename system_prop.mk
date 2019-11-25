cae7912172b5e85a473e2cb570ac68c626880e88
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-11-25 19:29:12 +0100

    g3ds: Remove unnecessary slash

--- system_prop.mk
+++ system_prop.mk
@@ -7,4 +7,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
     ro.telephony.call_ring.multiple=0 \
     ro.multisim.set_audio_params=true \
-    persist.radio.multisim.config=dsda \
+    persist.radio.multisim.config=dsda

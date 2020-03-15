60fbd147bb52a78e54a88257a28b741562c5a206
LuK1337 <priv.luk@gmail.com>
2020-03-15 15:13:10 +0100

    g3ds: init: Override all the things
    Change-Id: I1afcdc247425f39e6802b5d652ed65d267da6bd1

--- init/init_g3ds.cpp
+++ init/init_g3ds.cpp
@@ -1,5 +1,6 @@
 /*
-   Copyright (c) 2016, The CyanogenMod Project
+   Copyright (C) 2016, The CyanogenMod Project
+   Copyright (C) 2017-2020, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -25,9 +26,10 @@
    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
    OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+*/
 
-#include <stdlib.h>
+#include <vector>
+
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
@@ -40,22 +42,24 @@
 using android::base::GetProperty;
 using android::init::property_set;
 
-void property_override(char const prop[], char const value[])
+std::vector<std::string> ro_product_props_default_source_order = {
+    ".",
+    "product.",
+    "product_services.",
+    "odm.",
+    "vendor.",
+    "system.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
 
     pi = (prop_info*) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void property_override_triple(char const product_prop[], char const system_prop[], char const vendor_prop[], char const value[])
-{
-    property_override(product_prop, value);
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
 }
 
 void cdma_properties()
@@ -78,45 +82,67 @@ void vendor_load_properties()
     std::string serialno;
     std::string device;
 
+    const auto set_ro_build_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
     serialno = GetProperty("ro.boot.serialno", "");
 
     if (serialno == "D856") {
         /* CIS */
         gsm_properties("9,1");
         property_override("ro.build.description", "g3_open_cis-user 6.0 MRA58K 16228163339a2 release-keys");
-        property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "lge/g3_open_cis/g3:6.0/MRA58K/16228163339a2:user/release-keys");
-        property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "LG-D856");
-        property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "lg_d856");
+        for (const auto &source : ro_product_props_default_source_order ) {
+            set_ro_build_prop(source, "fingerprint", "lge/g3_open_cis/g3:6.0/MRA58K/16228163339a2:user/release-keys");
+            set_ro_product_prop(source, "device", "lg_d856");
+            set_ro_product_prop(source, "model", "LG-D856");
+        }
     } else if (serialno == "D857") {
         /* CHINA_OPEN_LTE */
         gsm_properties("20,1");
         property_override("ro.build.description", "g3_open_cn-user 5.0.1 LRX22G 152311653f5c9 release-keys");
-        property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "lge/g3_open_cn/g3:5.0.1/LRX22G/152311653f5c9:user/release-keys");
-        property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "LG-D857");
-        property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "lg_d857");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "lge/g3_open_cn/g3:5.0.1/LRX22G/152311653f5c9:user/release-keys");
+            set_ro_product_prop(source, "device", "lg_d857");
+            set_ro_product_prop(source, "model", "LG-D857");
+        }
     } else if (serialno == "D858HK") {
         /* HONGKONG */
         gsm_properties("20,1");
         property_override("ro.build.description", "g3_open_hk-user 6.0 MRA58K 160331641f312 release-keys");
-        property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "lge/g3_open_hk/g3:6.0/MRA58K/160331641f312:user/release-keys");
-        property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "LG-D858HK");
-        property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "lg_d858hk");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "lge/g3_open_hk/g3:6.0/MRA58K/160331641f312:user/release-keys");
+            set_ro_product_prop(source, "device", "lg_d858hk");
+            set_ro_product_prop(source, "model", "LG-D858HK");
+        }
     } else if (serialno == "D858") {
         /* CHINA_MOBILE_LTE */
         gsm_properties("20,1");
         property_override("ro.build.description", "g3_cmcc_cn-user 5.0.1 LRX22G 1523117098b82 release-keys");
-        property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "lge/g3_cmcc_cn/g3:5.0.1/LRX22G/1523117098b82:user/release-keys");
-        property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "LG-D858");
-        property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "lg_d858");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "lge/g3_cmcc_cn/g3:5.0.1/LRX22G/1523117098b82:user/release-keys");
+            set_ro_product_prop(source, "device", "lg_d858");
+            set_ro_product_prop(source, "model", "LG-D858");
+        }
     } else if (serialno == "D859") {
-       /* CHINA_TELECOM_LTE */
+        /* CHINA_TELECOM_LTE */
         cdma_properties();
         property_override("ro.build.description", "g3_ctc_cn-user 5.0.1 LRX22G 152311713704b release-keys");
-        property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "lge/g3_ctc_cn/g3:5.0.1/LRX22G/152311713704b:user/release-keys");
-        property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "LG-D859");
-        property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "lg_d859");
-    } 
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "lge/g3_ctc_cn/g3:5.0.1/LRX22G/152311713704b:user/release-keys");
+            set_ro_product_prop(source, "device", "lg_d859");
+            set_ro_product_prop(source, "model", "LG-D859");
+        }
+    }
 
     device = GetProperty("ro.product.device", "");
-    LOG(INFO) << "Found serialno id " << serialno.c_str() << "' setting build properties for '" << device.c_str() << "' device\n";
+    LOG(INFO) << "Found serialno id '" << serialno.c_str() << "'. Setting build properties for '" << device.c_str() << "'.\n";
 }
